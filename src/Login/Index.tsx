0d1f4e984596a98b34fb882c0495235afe9a60c6
José Miguel da Silveira Junior <miguel.s@malwee.com.br>
2020-07-21 02:48:26 -0300

    Configure semantic theming

--- src/Login/Index.tsx
+++ src/Login/Index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Input } from "semantic-ui-react";
 
 export const Login: React.FC = () => {
   return <Render></Render>;
 };
 
+const LoginInput = () => <Input icon="search" placeholder="Login" />;
+const PasswordInput = () => <Input icon="search" placeholder="Password" />;
+
 const Render: React.FC = () => {
   return (
     <div className="Login">
-      <h3>Login page</h3>
+      <LoginInput></LoginInput>
+      <PasswordInput></PasswordInput>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "semantic-ui-less/semantic.less";
 
 ReactDOM.render(
   <React.StrictMode>
